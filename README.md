e35722cd40a70666afd03137120c823237c13268
Gustavo-Falcao <duartegusfalcao@gmail.com>
2025-08-14 16:10:56 -0300

    Mudança no formato do projeto e estruturação do código.

--- README.md
+++ README.md
@@ -3,9 +3,17 @@
 - Implementar a lógica para cada opção no card
 - Colocar a descrição da tarefa como lista para ser visualizada na janela de info
 - Fazer uma função separada para inserir o conteudo na janela de info do card
-
+- Tirar o campo de subtarefa
+- fazer mais uma janela para a adicao das subtarefas
+- fazer uma função para engoblar todas as janelas e verificar qual está sendo mostrada
+    - Para essa função, pegar o conteudo do elemento para saber qual janela deve estar aberta
 
 ## Seta para o lado direito
 - Código html -> &#10095;
 - Código css -> \276F
 - Código Js -> \u276F
+
+## Objeto card
+- id
+- Nome
+- lista de tarefas
--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 //let lixeiras = document.querySelectorAll('.lixeira')
 
 class Card {
-    constructor(id, nome, conteudo) {
+    constructor(id, nome) {
         this.id = id;
         this.nome = nome;
-        this.conteudo = conteudo;
+        this.conteudo = [];
     }
 }
 
 let templates = []
 
+//Adicionar card
+document.getElementById('formulario').addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    const dados = new FormData(this)
+
+    criarCard(dados.get('nome-tarefa'))
+    limparCaixaText()
+    closeWidow()
+});
+
+function limparCaixaText() {
+    document.getElementById('text-box').value = ""
+}
+ 
 function submit() {
     console.log("Clicadoo")
     let caixaTxt = document.querySelector('#text-box')
@@ -21,107 +36,105 @@ function submit() {
     } else if(caixaDesc.value.length == 0) {
         alert('ERROR=>[Nenhum identificador informado!!]')
     } else {
-        let hora = new Date
-        let horaString = hora.getHours().toString() + hora.getMinutes().toString() + hora.getSeconds().toString()
-
-        //Criação do Card
-        let card = document.createElement("div")
-        card.setAttribute("class", "card")
-        card.setAttribute("id", horaString)
-        
-        //Criação do sub-card
-        let subCard = document.createElement("div")
-        subCard.setAttribute("class", "sub-card")
-
-        //Criação do Nome da tarefa
-        let text = document.createElement("p")
-        text.textContent = caixaTxt.value
-
-        //Criação do objeto Card
-        templates.push(new Card(horaString, caixaTxt.value, caixaDesc.value))
-        //Criação da seta para baixo
-        let setaParaBaixo = document.createElement("span")
-        setaParaBaixo.setAttribute("class", "para-baixo")
-        setaParaBaixo.innerHTML = "&#10095"
-        
-        //Criação da caixa com as opções
-        let boxOptions = document.createElement("div")
-        boxOptions.setAttribute("class", "options")
-
-        //Criação do icone de info
-        let infoIcon = document.createElement("span")
-        infoIcon.setAttribute("class", "material-symbols-outlined icones")
-        infoIcon.textContent = "info"
-
-        //Criação do icone de editar
-        let editIcon = document.createElement("span")
-        editIcon.setAttribute("class", "material-symbols-outlined icones")
-        editIcon.textContent = "edit"
-
-        //Criação do icone de mover item
-        let moveIcon = document.createElement("span")
-        moveIcon.setAttribute("class", "material-symbols-outlined icones")
-        moveIcon.textContent = "move_item"
-
-        //Criação do icone de deletar
-        let deleteIcon = document.createElement("span")
-        deleteIcon.setAttribute("class", "material-symbols-outlined icones")
-        deleteIcon.textContent = "delete"
-        
-        //Adicionando conteudos no subcard
-        subCard.appendChild(text)
-        subCard.appendChild(setaParaBaixo)
-
-        //Adicionando icones nas opcoes
-        boxOptions.appendChild(infoIcon)
-        boxOptions.appendChild(editIcon)
-        boxOptions.appendChild(moveIcon)
-        boxOptions.appendChild(deleteIcon)
-
-        //Adicionando elementos no card
-        card.appendChild(subCard)
-        card.appendChild(boxOptions)
-
-        let caixa = document.querySelector('#box-todo')
-        caixa.appendChild(card)
-        caixaTxt.value = ""
-        caixaDesc.value = ""
-        messageForNoTasks()
+        criarCard(caixaTxt, caixaDesc)
         closeWidow()
-
-        //Funcao para apagar o card
-        // icones.forEach(icone => {
-        //     icone.addEventListener('click', (element) => {
-        //         let infoIcone = element.target
-        //         console.log("classe =>", infoIcone.className)
-        //         console.log("Conteudo dentro => ", infoIcone.textContent)
-        //         let cardDoIcone = infoIcone.closest('.card')
-        //         let idCardIcone = cardDoIcone.id
-        //         console.log("Id do pai=>", idCardIcone)
-        //         removeTask(idPaiIcone)
-        //     });
-        // });
-
-
-        /* setaExpandirIcones = document.querySelectorAll('.para-baixo')
-        console.log("Quantidade Seta =>", setaExpandirIcones.length)
-
-        //Mostrar opcoes do card
-        setaExpandirIcones.forEach(seta => {
-            seta.addEventListener('click', (event) => {
-                let elemento = event.target
-                elemento.classList.toggle('para-cima')
-
-                let card = elemento.closest('.card')
-                card.classList.toggle('expandir-card')
-
-                let option = card.querySelector('.options')
-                option.classList.toggle('mostrar-options')
-            });
-        }); */
-        
-
     }
+}
+
+function gerarId() {
+    let hora = new Date
+    return hora.getHours().toString() + hora.getMinutes().toString() + hora.getSeconds().toString()
+}
+
+function criarCard(caixaNome) {
+// caixaTxt e caixaDesc
+    //Gerar id para o card
+    let horaString = gerarId()
+
+    //Criar onbjeto do card
+    criarObjetoCard(horaString, caixaNome)
+    
+    //Criar Card
+    let card = document.createElement("div")
+    card.setAttribute("class", "card")
+    card.setAttribute("id", horaString)
+    
+    //Criar subcard
+    let subCard = criarSubCard(caixaNome)
+
+    //Criar opcoes
+    let boxOptions = criarCaixaOpcoes()
+
+    //Adicionando elementos no card
+    card.appendChild(subCard)
+    card.appendChild(boxOptions)
+
+    let caixa = document.querySelector('#box-todo')
+    caixa.appendChild(card)
+    caixaNome.value = ""
+    //caixaDesc.value = ""
+    messageForNoTasks()
+
+}
+
+function criarCaixaOpcoes() {
+    //Criação da caixa com as opções
+    let boxOptions = document.createElement("div")
+    boxOptions.setAttribute("class", "options")
+
+    //Criação do icone de info
+    let infoIcon = document.createElement("span")
+    infoIcon.setAttribute("class", "material-symbols-outlined icones")
+    infoIcon.textContent = "info"
+
+    //Criação do icone de editar
+    let editIcon = document.createElement("span")
+    editIcon.setAttribute("class", "material-symbols-outlined icones")
+    editIcon.textContent = "edit"
+
+    //Criação do icone de adicionar
+    let addIcon = document.createElement("span")
+    addIcon.setAttribute("class", "material-symbols-outlined icones")
+    addIcon.textContent = "add"
+
+    //Criação do icone de deletar
+    let deleteIcon = document.createElement("span")
+    deleteIcon.setAttribute("class", "material-symbols-outlined icones")
+    deleteIcon.textContent = "delete"
+
+    //Adicionando icones na caixa opcoes
+    boxOptions.appendChild(infoIcon)
+    boxOptions.appendChild(editIcon)
+    boxOptions.appendChild(addIcon)
+    boxOptions.appendChild(deleteIcon)
+
+    return boxOptions
+}
+
+function criarObjetoCard(id, caixaNome) {
+    //Criação do objeto Card
+    templates.push(new Card(id, caixaNome))
+}
+
+function criarSubCard(caixaNome) {
+    //Criação do sub-card
+    let subCard = document.createElement("div")
+    subCard.setAttribute("class", "sub-card")
+
+    //Criação do Nome da tarefa
+    let text = document.createElement("p")
+    text.textContent = caixaNome
+
+    //Criação da seta para baixo
+    let setaParaBaixo = document.createElement("span")
+    setaParaBaixo.setAttribute("class", "para-baixo")
+    setaParaBaixo.innerHTML = "&#10095"
+
+    //Adicionando conteudos no subcard
+    subCard.appendChild(text)
+    subCard.appendChild(setaParaBaixo)
+
+    return subCard
 }
 
 const caixaToDo = document.getElementById('box-todo')
@@ -148,11 +161,29 @@ caixaToDo.addEventListener('click', function (event) {
             case "info":
                 mostrarInfoCard(eventoClicado)
             break
+            case "add":
+
             default: return
         }
         }  
     }
 });
+
+//Mostrar janela e esconder o restante
+function mostrarPopUp(elemento) {
+    let idElemento = elemento.id
+    let janelas = document.querySelectorAll('.janela-pop')
+
+    janelas.forEach(janela => {
+        if(janela.id !== idElemento) {
+            if(templateEstaSendoMostrado(janela)) {
+                esconderTemplate(janela)
+            }
+        }
+    });
+
+    mostrarTemplate(elemento)
+}
 
 function mostrarInfoCard(elemento) {
     let janelaInfo = document.getElementById('janela_info')
@@ -194,35 +225,30 @@ function mostrarIconesCard(elemento) {
 
 //Pega a informacao do card em qual o clic ocorreu e remove o card
 function removerElemento(elemento) {
-    let nomeIcone = elemento.textContent
-    console.log(`Nome do elemento => ${nomeIcone}`)
-    if(nomeIcone == "delete") {
-        let infoCard = elemento.closest('.card')
-        console.log(`Id do card => ${infoCard.id}`)
-        removeTask(infoCard.id)
-    } 
+    console.log(`Nome do elemento => ${elemento.textContent}`)
+    let infoCard = elemento.closest('.card')
+    console.log(`Id do card => ${infoCard.id}`)
+    removeTask(infoCard.id)
 }
 
 //Mostrar menssagem no campo se não tiver card
 function messageForNoTasks() {
-    let tasksBoxes = document.querySelectorAll('.box')
-    console.log('Quantidade das caixas => ' + tasksBoxes.length)
+    
+    let box = document.getElementById('box-todo')
 
-    tasksBoxes.forEach(box => {
-        let cards = box.querySelectorAll('.card')
-        console.log('Quantidade cards => ', cards.length)
-        if(cards.length === 0 && !box.querySelector('.msg')) {
-            let msg = document.createElement("p")
-            msg.textContent = "No cards yet"
-            msg.classList.add("msg")
-            box.appendChild(msg)
-            console.log('Criou paragrafo')
-        } else if(cards.length > 0 && box.querySelector('.msg')){
-            let msgToRemove = box.querySelector('.msg')
-            box.removeChild(msgToRemove)
-            console.log('Removeu paragrafo')
-        }
-    });
+    let cards = box.querySelectorAll('.card')
+    console.log('Quantidade cards => ', cards.length)
+    if(cards.length === 0 && !box.querySelector('.msg')) {
+        let msg = document.createElement("p")
+        msg.textContent = "No cards yet"
+        msg.classList.add("msg")
+        box.appendChild(msg)
+        console.log('Criou paragrafo')
+    } else if(cards.length > 0 && box.querySelector('.msg')){
+        let msgToRemove = box.querySelector('.msg')
+        box.removeChild(msgToRemove)
+        console.log('Removeu paragrafo')
+    }
 }
 
 //Remove um elemento pelo seu id
@@ -256,6 +282,7 @@ function templateEstaSendoMostrado(elemento) {
     return elemento.classList.contains('show-janela') ? true : false
 }
 
+//Adicionar card
 document.getElementById('bot_addCard').addEventListener('click', function(e) {
     let button = e.target
     if(button) {
